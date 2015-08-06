084fab395e93e8f7895b3f8b9a449bd10cc9d1fc
Agustin <agustin.mende@thepasto.com>
2015-08-06 19:55:14 -0300

    Cosita slindas

---
+++ Assets/auto1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class auto1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Auto1"), "time", 20));
+	}
+	
+
+}
